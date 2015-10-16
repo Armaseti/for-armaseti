0aaadf8df5d53e61dcd0fe0419695c17920c7cf8
Armaseti <ruslan.armaseti@yandex.ru>
2015-10-16 14:25:29 +0700

    Update MyCode.js

--- MyCode.js
+++ MyCode.js
@@ -1,6 +1,32 @@
 var geoip2=function(){"use strict";function Lookup(successCallback,errorCallback,options,type){this.successCallback=successCallback,this.errorCallback=errorCallback,this.geolocation=options&&options.hasOwnProperty("geolocation")?options.geolocation:navigator.geolocation,this.type=type}var exports={};Lookup.prototype.returnSuccess=function(location){this.successCallback&&"function"==typeof this.successCallback&&this.successCallback(this.fillInObject(this.objectFromJSON(location)))},Lookup.prototype.returnError=function(error){this.errorCallback&&"function"==typeof this.errorCallback&&(error||(error={error:"Unknown error"}),this.errorCallback(error))},Lookup.prototype.objectFromJSON=function(json){return"undefined"!=typeof window.JSON&&"function"==typeof window.JSON.parse?window.JSON.parse(json):eval("("+json+")")};var fillIns={country:[["continent","Object","names","Object"],["country","Object","names","Object"],["registered_country","Object","names","Object"],["represented_country","Object","names","Object"],["traits","Object"]],city:[["city","Object","names","Object"],["continent","Object","names","Object"],["country","Object","names","Object"],["location","Object"],["postal","Object"],["registered_country","Object","names","Object"],["represented_country","Object","names","Object"],["subdivisions","Array",0,"Object","names","Object"],["traits","Object"]]};return Lookup.prototype.fillInObject=function(obj){for(var fill="country"===this.type?fillIns.country:fillIns.city,i=0;i<fill.length;i++)for(var path=fill[i],o=obj,j=0;j<path.length;j+=2){var key=path[j];o[key]||(o[key]="Object"===path[j+1]?{}:[]),o=o[key]}try{Object.defineProperty(obj.continent,"continent_code",{enumerable:!1,get:function(){return this.code},set:function(value){this.code=value}})}catch(e){obj.continent.code&&(obj.continent.continent_code=obj.continent.code)}if("country"!==this.type)try{Object.defineProperty(obj,"most_specific_subdivision",{enumerable:!1,get:function(){return this.subdivisions[this.subdivisions.length-1]},set:function(value){this.subdivisions[this.subdivisions.length-1]=value}})}catch(e){obj.most_specific_subdivision=obj.subdivisions[obj.subdivisions.length-1]}return obj},Lookup.prototype.getGeoIPResult=function(){var param,request,that=this,uri="//js.maxmind.com/geoip/v2.1/"+this.type+"/me?",httpParams={referrer:location.protocol+"//"+location.hostname};if(!this.alreadyRan){this.alreadyRan=1,"Microsoft Internet Explorer"===navigator.appName&&window.XDomainRequest&&-1===navigator.appVersion.indexOf("MSIE 1")?(request=new XDomainRequest,uri=window.location.protocol+uri,request.onprogress=function(){}):(request=new window.XMLHttpRequest,uri="https:"+uri);for(param in httpParams)httpParams.hasOwnProperty(param)&&httpParams[param]&&(uri+=param+"="+encodeURIComponent(httpParams[param])+"&");uri=uri.substring(0,uri.length-1),request.open("GET",uri,!0),request.onload=function(){if("undefined"==typeof request.status||200===request.status)that.returnSuccess(request.responseText);else{var error,contentType=request.hasOwnProperty("contentType")?request.contentType:request.getResponseHeader("Content-Type");if(/json/.test(contentType)&&request.responseText.length)try{error=that.objectFromJSON(request.responseText)}catch(e){error={code:"HTTP_ERROR",error:"The server returned a "+request.status+" status with an invalid JSON body."}}else error=request.responseText.length?{code:"HTTP_ERROR",error:"The server returned a "+request.status+" status with the following body: "+request.responseText}:{code:"HTTP_ERROR",error:"The server returned a "+request.status+" status but either the server did not return a body or this browser is a version of Internet Explorer that hides error bodies."};that.returnError(error)}},request.ontimeout=function(){that.returnError({code:"HTTP_TIMEOUT",error:"The request to the GeoIP2 web service timed out."})},request.onerror=function(){that.returnError({code:"HTTP_ERROR",error:"There was a network error receiving the response from the GeoIP2 web service."})},request.send(null)}},exports.country=function(successCallback,errorCallback,options){var l=new Lookup(successCallback,errorCallback,options,"country");l.getGeoIPResult()},exports.city=function(successCallback,errorCallback,options){var l=new Lookup(successCallback,errorCallback,options,"city");l.getGeoIPResult()},exports.insights=function(successCallback,errorCallback,options){var l=new Lookup(successCallback,errorCallback,options,"insights");l.getGeoIPResult()},exports}();
 var data_from_ip = {"ip":"","country_code":"","country_name":"","region_code":"","region_name":"","city":"","zip_code":"","time_zone":"","latitude":"","longitude":"","metro_code":""};
 var company;
+var telef;
+  var mail;
+  var katalog_LD;
+  var katalog_strizh;
+  var price;
+var showpopup=1;
+var yaCount = 0;
+var yaCountwith = 0;
+var bpress;
+var QueryString = function () {    
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("="); 
+    if (typeof query_string[pair[0]] === "undefined"){
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
 var onSuccess = function(location){
 	data_from_ip.ip=location.traits.ip_address;
 	data_from_ip.country_code=location.country.iso_code;
@@ -8,6 +34,7 @@ var onSuccess = function(location){
 	data_from_ip.city=location.city.names.ru;
 	data_from_ip.time_zone=location.location.time_zone;
 	company = whatcompany(data_from_ip);
+	checkpam();
 };
 var onError = function(error){
 scriptRequest('http://freegeoip.net/json/', ip_data_succ, ip_data_fail);
@@ -64,32 +91,7 @@ mcsub(block_id,name_of_lid,mail_of_lid);
    });	
 }
 //-----------------------------------
-var telef;
-  var mail;
-  var katalog_LD;
-  var katalog_strizh;
-  var price;
-var showpopup=1;
-var yaCount = 0;
-var yaCountwith = 0;
-var bpress;
-var QueryString = function () {    
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("="); 
-    if (typeof query_string[pair[0]] === "undefined"){
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  } 
-    return query_string;
-}();
+
 function SetInterface(param){ 
 switch (param) { // распределяет ссылки и значения для доп параметров
   case 'krasnoyarsk':
@@ -244,7 +246,7 @@ function scriptRequest(url, onSuccess, onError) {
   	script.src = url;
 	document.body.appendChild(script);
 }
-function ip_data_succ(data) {data_from_ip = data; company = whatcompany(data)} ;
+function ip_data_succ(data) {data_from_ip = data; company = whatcompany(data); checkpam(); } ;
 function ip_data_fail(){company = "Армасети"};
 
 //--------------------------------------------
@@ -352,7 +354,6 @@ function mcupdate(){
 function mcsub(block_id,name_widget, mail_widget){
 	var mcfname = $(block_id).find('input[name="'+name_widget+'"]').val(); 
 	var mcemail = $(block_id).find('input[name="'+mail_widget+'"]').val(); 
-	var company = whatcompany(data_from_ip);
   if (emailAddressValid(mcemail)){
 	fetch('https://us11.api.mailchimp.com/2.0/lists/subscribe', {
 		  mode: 'no-cors',
@@ -436,10 +437,10 @@ if (QueryString.source){
 	}
 SetInterface(getCookie('source'));
 
-if (QueryString.mcmail){
+function checkpam() {if (QueryString.mcmail){
 	setCookie('from_mail','yepi', {expires: life_time_mail.toUTCString(), path:"/"} );
 	if (QueryString.mcfname){mcsubi()}
-	else{mcupdate();}}
+	else{mcupdate();}}}
 if (getCookie('from_mail') == 'yepi'){showpopup=0};
 	
 
