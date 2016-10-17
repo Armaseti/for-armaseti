e3419c6ecbbd44460b49efe307a14188865b520e
Armaseti <ruslan.armaseti@yandex.ru>
2016-10-18 01:17:58 +0700

    Добавление счетчика 39800615

--- MyCode.js
+++ MyCode.js
@@ -24,6 +24,32 @@
 })(document, window, "yandex_metrika_callbacks");
 var divv = document.createElement('noscript'); divv.innerHTML = '<div><img src="https://mc.yandex.ru/watch/33144723" style="position:absolute; left:-9999px;" alt="" /></div>';
 document.body.appendChild(divv);
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter39800615 = new Ya.Metrika({
+                id:39800615,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+var divv = document.createElement('noscript'); divv.innerHTML = '<div><img src="https://mc.yandex.ru/watch/33144723" style="position:absolute; left:-9999px;" alt="" /></div>';
+document.body.appendChild(divv);
 //------------------------
 (function(){ var widget_id = 'jefUEST2ps';
     var s = document.createElement('script');
